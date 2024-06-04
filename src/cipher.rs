ef5d58906df95740a7828a7748b8aa552446dfde
Radu Marias <radumarias@gmail.com>
2024-06-04 20:20:54 +0300

    fix keyboard typo

--- src/cipher.rs
+++ src/cipher.rs
@@ -4,5 +4,5 @@ trait Cipher {
     fn seal_in_place<'a>(plaintext: &'a mut [u8], block_index: Option<u64>, aad: Option<&[u8]>, nonce: Option<&[u8]>,
                          tag_out: &mut [u8], nonce_out: Option<&mut [u8]>) -> io::Result<&'a [u8]>;
 
-    fn open_in_place<'a>(ciphertext_and_tag: &'a mut [u8], block_index: Option<u64>, aad: Option<&[u8]>, no     nce: &[u8]) -> io::Result<&'a mut [u8]>;
+    fn open_in_place<'a>(ciphertext_and_tag: &'a mut [u8], block_index: Option<u64>, aad: Option<&[u8]>, nonce: &[u8]) -> io::Result<&'a mut [u8]>;
 }
