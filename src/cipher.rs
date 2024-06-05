e8824ad9001a6eeab6540d8a388aa9a158f4cc1b
Radu Marias <radumarias@gmail.com>
2024-06-06 02:03:15 +0300

    add allow_dead_code

--- src/cipher.rs
+++ src/cipher.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+#[allow(dead_code)]
 trait Cipher {
     fn seal_in_place<'a>(plaintext: &'a mut [u8], block_index: Option<u64>, aad: Option<&[u8]>, nonce: Option<&[u8]>,
                          tag_out: &mut [u8], nonce_out: Option<&mut [u8]>) -> io::Result<&'a [u8]>;
