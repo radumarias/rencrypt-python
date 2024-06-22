3180c03830af0b3a82416146a57f54d1d547ca6d
Radu Marias <radumarias@gmail.com>
2024-06-22 15:06:04 +0300

    Update tests.bat

--- tests.bat
+++ tests.bat
@@ -1,5 +1,14 @@
-pytest|| exit /b
-python examples/encrypt.py || exit /b
-python examples/encrypt_from.py || exit /b
-python examples/encrypt_file.py || exit /b
-python benches/bench.py || exit /b
+pytest
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+python examples/encrypt.py
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+python examples/encrypt_from.py
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+python examples/encrypt_file.py
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+python benches/bench.py
+if %errorlevel% neq 0 exit /b %errorlevel%
